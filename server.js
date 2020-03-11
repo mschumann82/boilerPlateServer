6fa48765a4fa2576aa80ae3eba4a7a8c945be818
Michael Schumann <michael.s@mstsolutions.com>
2020-03-11 15:14:19 -0700

    stuff for tim

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const fs = require('fs');
+const file = fs.readFileSync("public/text.txt").toString('utf-8');
 
 
 // Sets up the Express App
@@ -23,3 +25,18 @@ app.use('/', htmlRoutes);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+
+  /////////////////
+
+let arrayOfText = file.split(',');
+console.log(arrayOfText.length);
+let tfa = [];
+
+arrayOfText.map(item =>  {
+    if (item.includes('tfa')) {
+        tfa.push(item);
+    };
+    console.log(tfa);
+});
+
